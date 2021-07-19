5d3a0e5cc61d000ec195617a814023241fe1898f
smna <ghimiresamana666@gmail.com>
2021-07-19 17:37:48 +0545

    services page--new

--- src/pages/Services1/add.js
+++ src/pages/Services1/add.js
@@ -4,6 +4,17 @@ import Grid from "@material-ui/core/Grid";
 import { Typography } from "@material-ui/core";
 import ZoneApiList from "../../services/ZoneApi";
 import TutorialDataService from "../../services/TutorialService";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    // width: 500,
+    "& > * + *": {
+      marginTop: theme.spacing(1),
+    },
+  },
+}));
 const AddServices = (props) => {
   const initialTutorialState = {
     id: "",
@@ -32,6 +43,7 @@ const AddServices = (props) => {
     setTutorial((prevTutorial)=>{ 
       return {...prevTutorial, [name]: value} 
     });
+  }
 
   const handleInputZone = (event, option) => {
     setZone(
